24b2206cce8fa3cda87f121d41890a3ef49dc73b
Mohamed Hassan <73446264+MohamedOne@users.noreply.github.com>
2021-07-19 17:50:45 -0400

    elementary card skeleton

---
+++ models/Post.ts
@@ -0,0 +1,10 @@
+
+interface Post {
+    displayImg: string,
+    displayName: string,
+    userName: string,
+    postBody: string,
+    numLikes: number,
+    timeStamp: string,
+    comments: string[]
+}
