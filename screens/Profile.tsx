d4b8302e9db5e35ec9102b44234d014f5ea7942a
Mohamed Hassan <73446264+MohamedOne@users.noreply.github.com>
2021-07-28 21:34:57 -0400

    connecting profile endpoints

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -3,9 +3,28 @@ import { Text, View, StyleSheet, Image, SafeAreaView, Pressable, FlatList } from
 import { Card } from 'react-native-elements';
 import PostCard from './PostCard';
 import { ScrollView } from 'react-native-gesture-handler';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 
-const Profile = () => {
+const Profile = (props: any) => {
+
+    //Grab user posts of this specific user
+    let userPostArray = []
+    const grabUserSpecificPosts = async() => {
+        await axios.get(`https://w822121nz1.execute-api.us-east-2.amazonaws.com/Prod/post/${props.profileInfo.userName}/specific`, {
+            headers : {
+                Authorization : "TokenToBePulledFromState"
+            }
+        }).then(resp => {
+            //Response is an array of posts 
+            resp.data.forEach((elem: any) => {
+                userPostArray.push(elem);
+            })
+        })
+
+    }
 
     const [postCards, setPostCards] = useState([
         {
@@ -66,7 +85,7 @@ const Profile = () => {
                     > 
                         <View style={styles.imageContainer}>
                             <Image
-                            source={require('../assets/images/illuminati.png')}
+                            source={props.profileInfo.profileImg}
                             style={styles.image}
                             />
                         </View>
@@ -75,13 +94,13 @@ const Profile = () => {
                         <View style={styles.infoContainer}>
                             <Text 
                                 style={styles.displayName}
-                            >Hello</Text>
+                            >{props.profileInfo.displayName}</Text>
                             <Text
                                 style={styles.username}
-                            >@God</Text>
+                            >{props.profileInfo.userName}</Text>
                             <Text
                                 style={styles.email}
-                            >Email</Text>     
+                            >{props.profileInfo.email}</Text>     
                         </View>
 
                         <View >
