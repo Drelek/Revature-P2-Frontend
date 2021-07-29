8f3df97a6b5ec32aa9bde40f71799b6cf331694e
Jared Burkamper <jaredburkamper@gmail.com>
2021-07-29 10:12:25 -0500

    Removed extra file

--- Cognito.ts
+++
@@ -1,5 +0,0 @@
-import { CognitoIdentityProviderClient } from "@aws-sdk/client-cognito-identity-provider";
-
-const cogClient = new CognitoIdentityProviderClient({region: "us-east-2"});
-
-export default cogClient;
