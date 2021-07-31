a01c984758c09aaec3a233ec3aaccea28b894429
Samuel Smetzer <kamuela.1300@gmail.com>
2021-07-31 15:06:54 -0700

    s

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,15 +1,46 @@
 import axios from 'axios';
 import React from 'react'
 import { useState } from 'react';
-import { View, SafeAreaView, TextInput, Pressable, StyleSheet, Text } from 'react-native';
+import { View, TextInput, Pressable, StyleSheet, Text, Image } from 'react-native';
+import { Card } from 'react-native-elements';
+import { FlatList } from 'react-native-gesture-handler';
 import { useSelector } from 'react-redux';
 import { IAppState } from '../redux/store';
+import PostCard from './PostCard';
 
 
 const SearchScreen: React.FC = (props: any) => {
 
-    const [search, setSearch] = useState(' ');
     const token = useSelector((state: IAppState) => state.auth.AccessToken);
+    const user = useSelector((store: IAppState) => store.user);
+
+    const [search, setSearch] = useState(' ');
+    //const [searchedUsers, setUsers] = useState(' ')
+    const searchedUsers = [
+        {
+            userName: 'theSponge',
+            displayName: 'captain',
+            profileImg: user?.profileImg,
+            followers: ["sam", "mo", "kai", "jared"]
+        },
+        {
+
+            userName: "admin",
+            displayName: "no name",
+            profileImg: user?.profileImg,
+            followers: ["sam", "mo", "kai", "jared"]
+
+        },
+        {
+
+            userName: "joe",
+            displayName: "mama",
+            profileImg: user?.profileImg,
+            followers: ["sam", "mo", "kai", "jared"]
+
+        }
+
+    ]
 
     async function Search() {
 
@@ -26,18 +57,43 @@ const SearchScreen: React.FC = (props: any) => {
     return (
 
         <View style={styles.container}>
-            <View style={styles.inputContainer}>
-                <TextInput
-                    placeholder="Search"
-                    placeholderTextColor="white"
-                    style={styles.inputBox}
-                    onChangeText={setSearch} />
-            </View>
+            <View style={styles.topContainer}>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        placeholder="Search"
+                        placeholderTextColor="white"
 
-            <View style={styles.buttonContainer}>
-                <Pressable style={styles.pressable} onPress={() => Search()}>
-                    <Text style={styles.text}>Search</Text>
-                </Pressable>
+                        onChangeText={setSearch} />
+                </View>
+                <View style={styles.buttonContainer}>
+                    <Pressable style={styles.pressable} onPress={() => Search()}>
+                        <Text style={styles.text}>Search</Text>
+                    </Pressable>
+                </View>
+            </View>
+            <View>
+                <FlatList data={searchedUsers}
+                    renderItem={({ item }) => <Card>
+                        <View >
+                            <Image
+                                source={{ uri: `${item.profileImg}` }}
+                                style={styles.image}
+                            />
+                        </View>
+                        <View>
+                            <Text
+                                style={styles.displayName}
+                            >{item.displayName}</Text>
+                            <Text
+                                style={styles.username}
+                            >{item.userName}</Text>
+                            <Text>
+                                {item.followers.size}
+                            </Text>
+                        </View>
+
+                    </Card>}
+                    keyExtractor={item => item.userName} />
             </View>
         </View>
     );
@@ -52,52 +108,68 @@ const styles = StyleSheet.create({
     text: {
         fontSize: 14,
         color: "white",
+        marginTop: 0,
+
+
     },
 
 
     container: {
-        flexDirection: 'row',
+        flex: 1,
         justifyContent: 'center',
     },
-
-    inputBox: {
-        color: "white",
-        fontSize: 16,
-        flexDirection: "row",
-        justifyContent: "center",
-        textAlignVertical: 'top',
-        paddingVertical: 15,
-        paddingHorizontal: 5,
-        marginLeft: 10,
+    topContainer: {
+        flex: 1,
+        flexDirection: "row"
 
     },
 
     buttonContainer: {
-        flex: 1,
-        flexDirection: "row",
+
         justifyContent: "flex-end",
-        alignItems: "center",
+        alignItems: "flex-end",
     },
 
     inputContainer: {
-        flex: 4,
-        marginBottom: 10,
+
         backgroundColor: 'rgb(42,45,47)',
-        borderRadius: 20,
+        borderRadius: 10,
         justifyContent: "center",
         alignContent: "center",
-        marginTop: 20,
         marginLeft: 10,
-        marginRight: 10
+
+
     },
 
     pressable: {
+
         backgroundColor: "purple",
         paddingHorizontal: 10,
         paddingVertical: 15,
-        marginBottom: 10,
-        borderRadius: 15,
-        marginTop: 20
-    }
+        borderRadius: 20,
+        width: 70,
+
+
+    },
+
+    image: {
+        width: 100,
+        height: 100,
+        borderRadius: 100,
+        backgroundColor: "purple"
+    },
+    displayName: {
+        fontWeight: "bold",
+        fontSize: 22,
+        color: "white",
+        paddingLeft: 15,
+        marginBottom: 5
+    },
+    username: {
+        fontSize: 18,
+        color: "white",
+        paddingLeft: 15,
+        marginBottom: 5
+    },
 })
 
