18ef0ec6d2048f9523f88fa75ed5b8e0b64441af
Samuel Smetzer <kamuela.1300@gmail.com>
2021-07-31 17:41:07 -0700

    some stuff

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -11,58 +11,17 @@ import UserCard from '../components/UserCard';
 const SearchScreen: React.FC = (props: any) => {
 
     const [search, setSearch] = useState(' ');
-    const [working, setWorking] = useState(false);
     const [results, setResults] = useState([]);
     const token = useSelector((state: IAppState) => state.auth.AccessToken);
     const user = useSelector((store: IAppState) => store.user);
 
-    //const [searchedUsers, setUsers] = useState(' ')
-    const searchedUsers = [
-        {
-            userName: 'theSponge',
-            displayName: 'captain',
-            profileImg: user?.profileImg,
-            followers: "100"
-        },
-        {
 
-            userName: "admin",
-            displayName: "no name",
-            profileImg: user?.profileImg,
-            followers: "1"
 
-        },
-        {
-
-            userName: "joe",
-            displayName: "mama",
-            profileImg: user?.profileImg,
-            followers: "10"
-
-        },
-        {
-
-            userName: "asdadsxz",
-            displayName: "mama",
-            profileImg: user?.profileImg,
-            followers: "50"
-
-        },
-        {
-
-            userName: "asdas",
-            displayName: "mama",
-            profileImg: user?.profileImg,
-            followers: ["sam", "mo", "kai", "jared"]
-
-        }
-
-    ]
 
     async function Search() {
         console.log(search);
         try {
-            // setWorking(true);
+
             const res = await axios.get(`https://w822121nz1.execute-api.us-east-2.amazonaws.com/Prod/user/search/${search}`, { headers: { Authorization: token } });
             console.log(res.data);
             setResults(res.data[0]);
@@ -134,18 +93,6 @@ const styles = StyleSheet.create({
         marginTop: 20,
         marginBottom: 13
     },
-    userContainer: {
-
-        flexDirection: "row",
-        justifyContent: 'center',
-        backgroundColor: 'rgb(33, 37, 41)',
-        borderRadius: 10,
-        borderColor: 'purple',
-        borderWidth: 5,
-        marginBottom: 15,
-        marginTop: 15,
-        marginHorizontal: 15,
-    },
 
     inputContainer: {
         backgroundColor: 'rgb(42,45,47)',
@@ -170,31 +117,6 @@ const styles = StyleSheet.create({
 
     },
 
-    image: {
-        width: 100,
-        height: 100,
-        borderRadius: 100,
-        backgroundColor: "purple",
-        marginRight: 10,
-        marginTop: 10,
-        marginBottom: 10,
-        justifyContent: "flex-start",
-        alignItems: "flex-start",
 
-    },
-    displayName: {
-        fontWeight: "bold",
-        fontSize: 22,
-        color: "white",
-        paddingLeft: 15,
-        marginBottom: 5,
-
-    },
-    username: {
-        fontSize: 18,
-        color: "white",
-        paddingLeft: 15,
-        marginBottom: 5
-    },
 })
 
