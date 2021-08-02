fdb059505aeb1de951d1b31535a47c0800252cc9
Jared Burkamper <jaredburkamper@gmail.com>
2021-08-01 19:09:45 -0500

    Names on search screen now take you to profile

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,40 +1,45 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { View, Image, Text, StyleSheet } from "react-native";
+import { View, Image, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { Card } from 'react-native-elements'
 
 const UserCard = (props: any) => {
 
+    const navigation = useNavigation()
+
     return (
-        
+
         <Card containerStyle={styles.userContainer}>
-        <View style={{ flexDirection: "row" }}>
-            <View style={styles.imageContainer}>
-                <Image
-                source={{ uri: `${props.item.profileImg.S}` }}
-                style={styles.image} />
-            </View>
-            
-
-            <View style={styles.rightContainer}>
-                <View style={styles.names}>
-                    <Text
-                    style={styles.displayName}
-                    >{props.item.displayName.S}</Text>
-                    <Text
-                    style={styles.username}
-                    >{"@" + props.item.dataKey.S}</Text>
+            <View style={{ flexDirection: "row" }}>
+                <View style={styles.imageContainer}>
+                    <Image
+                        source={{ uri: `${props.item.profileImg.S}` }}
+                        style={styles.image} />
                 </View>
 
-                <View style={styles.follow}>
-                    <Text
-                    style={styles.username}>
-                    {`Followers: ${props.item.followers.SS.length - 1}`}</Text>
-                </View>
-        
-            </View>
 
-            
-        </View>
+                <View style={styles.rightContainer}>
+                    <View style={styles.names}>
+                        <TouchableOpacity onPress={() => navigation.navigate("Profile", { displayName: props.item.displayName.S, userName: props.item.dataKey.S })}>
+                            <Text
+                                style={styles.displayName}
+                            >{props.item.displayName.S}</Text>
+                        </TouchableOpacity>
+                        <Text
+                            style={styles.username}
+                        >{"@" + props.item.dataKey.S}</Text>
+                    </View>
+
+                    <View style={styles.follow}>
+                        <Text
+                            style={styles.username}>
+                            {`Followers: ${props.item.followers.SS.length - 1}`}</Text>
+                    </View>
+
+                </View>
+
+
+            </View>
         </Card>
     )
 
@@ -42,18 +47,18 @@ const UserCard = (props: any) => {
 export default UserCard
 
 const styles = StyleSheet.create({
-    imageContainer:{
-        flex:1
+    imageContainer: {
+        flex: 1
     },
     rightContainer: {
-        flex:2
+        flex: 2
     },
-    names:{
-        flex:2
+    names: {
+        flex: 2
     },
 
-    follow:{
-        flex:1
+    follow: {
+        flex: 1
     },
 
     image: {
@@ -80,7 +85,7 @@ const styles = StyleSheet.create({
         marginBottom: 5
     },
     userContainer: {
-        flex:1,
+        flex: 1,
         justifyContent: 'center',
         backgroundColor: 'rgb(33, 37, 41)',
         borderRadius: 10,
